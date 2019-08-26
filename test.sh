38baf49821388bd1b3dc392fd3a0daa58d3e1ef4
aravind1635 <44403353+aravind1635@users.noreply.github.com>
2019-08-26 14:49:29 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "first argument = $1"
 echo "second argument = $2"
-echo "env variable ${GIT_USER_TO_CLONE}"
-echo "env variable ${GIT_PASSWORD_TO_CLONE}"
+echo "env variable ${GITHUB_USER_TO_CLONE}"
+echo "env variable ${GITHUB_PASSWORD_TO_CLONE}"
